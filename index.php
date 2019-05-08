a7a71184b7a1e75d48d88f3ad41daa81b26cf9f9
ini-username-ya <setix@net-list.com>
2019-05-08 21:19:18 +0700

    update

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
     <meta content="IE=edge" http-equiv="X-UA-Compatible">
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="Val" name="author">
-    <title>
-      Selamat datang di IMKAY
-    </title>
+    <title>Selamat datang di IMKAY</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
@@ -15,20 +13,16 @@
     <br>
     <div id="wrapshopcart">
       <div class="sidenav" id="mySidenav">
-        <a class="closebtn" href="javascript:void(0)" onclick="closeNav()">
-          &times;
-        </a>
-        <a href="/api">
-          API
-        </a>
-        <a href="/tools">
-          TOOLS
-        </a>
+        <a class="closebtn" href="javascript:void(0)" onclick="closeNav()">&times;</a>
+
+        <!-- item -->
+        <a href="/api">API</a>
+        <a href="/tools">TOOLS</a>
       </div>
+
       <!-- Use any element to open the sidenav -->
       <span onclick="openNav()" style="">
-        <i class="fa fa-bars">
-        </i>
+        <i class="fa fa-bars"></i>
       </span>
       <script>
         function openNav() {
@@ -38,28 +32,15 @@
           document.getElementById("mySidenav").style.width = "0";
         }
       </script>
+
       <center>
-        <h4>
-          <br>
-          Selamat datang di
-          <a href="#">
-            IMKAY
-          </a>
-        </h4>
+        <h4><br>Selamat datang di <a href="#">IMKAY</a></h4>
       </center>
       <hr>
       <center>
-        <p>
-          &copy; 2019&nbsp;
-          <a href="/">
-            IMKAY
-          </a>
-          &nbsp;
-          <i class="fa fa-facebook-official">
-          </i>
-          <a href="https://m.facebook.com/zvtyrdt.id">
-            zvtyrdt.id
-          </a>
+        <p>&copy; 2019 <a href="/">IMKAY</a>&nbsp;
+          <i class="fa fa-facebook-official"></i>
+          <a href="https://m.facebook.com/zvtyrdt.id">zvtyrdt.id</a>
         </p>
       </center>
     </div>
